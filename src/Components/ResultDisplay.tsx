63cdbd1100fb903407c502e80d53585d5ff4d74d
Ariyan Surena <AriyanSurena@gmail.com>
2025-11-11 19:05:49 +0330

    ResultDisplay Optimized.

--- src/Components/ResultDisplay.tsx
+++ src/Components/ResultDisplay.tsx
@@ -1,5 +1,3 @@
-import type React from "react";
-
 interface ResultDisplayProps {
     placeholder: string,
     label?: string,
@@ -12,23 +10,45 @@ const ResultDisplay: React.FC<ResultDisplayProps> = ({
     result
 }) => {
     const formattedResult = new Intl.NumberFormat().format(Number(result));
-    function handleCopy (value: (string | number)) {
-        navigator.clipboard.writeText(String(value))
+
+    const handleCopy: (value: string | number) => void = (value) => {
+        const stringValue = String(value).trim();
+
+        if (isNaN(Number(stringValue)) || !stringValue || stringValue === "0") {
+            return;
+        }
+
+        navigator.clipboard.writeText(stringValue);
     }
 
     return (
-        <section 
-            className={`flex flex-col gap-2 w-full text-center bg-slate-100 dark:bg-slate-600 hover:scale-[1.01] cursor-pointer rounded shadow p-2 my-2 ring-1 ring-slate-200 dark:ring-slate-700 overflow-hidden ${!result || result <= 0 && "opacity-50"}`}
-            onClick={ () => handleCopy(formattedResult) }
-            title={`Copy ${formattedResult}`}>
+        <section
+            className={`
+                flex flex-col gap-2 
+                w-full p-2 my-2 
+                text-center 
+                bg-slate-100 dark:bg-slate-600 
+                hover:scale-[1.01] 
+                cursor-pointer 
+                rounded shadow 
+                ring-1 ring-slate-200 dark:ring-slate-700 
+                overflow-hidden 
+                ${(!result || result <= 0) && "opacity-50 select-none cursor-auto hover:scale-[unset]"}
+            `}
+            onClick={
+                () => handleCopy(formattedResult)
+            }
+            title={
+                result ? `Copy ${formattedResult}` : ''
+            }>
             {
-                label 
-                ? (
-                    <span className="w-full text-center p-2 rounded bg-white dark:bg-slate-700 text-black dark:text-white select-none">
-                        {label}
-                    </span>
-                )
-                : null
+                label
+                    ? (
+                        <span className="w-full text-center p-2 rounded bg-white dark:bg-slate-700 text-black dark:text-white select-none">
+                            {label}
+                        </span>
+                    )
+                    : null
             }
             {
                 result >= 0
